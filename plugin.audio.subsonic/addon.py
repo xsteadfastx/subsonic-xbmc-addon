c361099cdb359f87a9ac9341ed56e3db54d4792f
Marvin Steadfast <xsteadfastx@gmail.com>
2014-07-18 09:17:44 +0200

    main rewrite with new features and new tests

--- plugin.audio.subsonic/addon.py
+++ plugin.audio.subsonic/addon.py
@@ -1,3 +1,4 @@
+from operator import itemgetter
 import sys
 import urllib
 import urlparse
@@ -38,28 +39,15 @@ class Subsonic(object):
         api_url = self.api('getMusicDirectory.view',
                            parameters={'id': id})
         r = requests.get(api_url)
-        albums = []
-        for album in r.json()['subsonic-response']['directory']['child']:
-            item = {}
-            item['artist'] = album['artist'].encode('utf-8')
-            item['title'] = album['title'].encode('utf-8')
-            item['id'] = album['id'].encode('utf-8')
-            albums.append(item)
 
-        return albums
+        return r.json()['subsonic-response']['directory']['child']
 
     def genre_list(self):
         api_url = self.api('getGenres.view')
         r = requests.get(api_url)
-        genres = []
-        for genre in r.json()['subsonic-response']['genres']['genre']:
-            item = {}
-            item['songCount'] = genre['songCount']
-            item['albumCount'] = genre['albumCount']
-            item['value'] = genre['value']
-            genres.append(item)
 
-        return genres
+        return sorted(r.json()['subsonic-response']['genres']['genre'],
+                      key=itemgetter('value'))
 
     def albums_by_genre_list(self, genre):
         api_url = self.api('getAlbumList.view',
@@ -67,15 +55,23 @@ class Subsonic(object):
                                        'genre': genre,
                                        'size': '500'})
         r = requests.get(api_url)
-        albums = []
-        for album in r.json()['subsonic-response']['albumList']['album']:
-            item = {}
-            item['artist'] = album['artist'].encode('utf-8')
-            item['title'] = album['title'].encode('utf-8')
-            item['id'] = album['id'].encode('utf-8')
-            albums.append(item)
 
-        return albums
+        return r.json()['subsonic-response']['albumList']['album']
+
+    def random_songs_by_genre(self, genre):
+        api_url = self.api('getRandomSongs.view',
+                           parameters={'size': '500',
+                                       'genre': genre})
+        r = requests.get(api_url)
+        return r.json()['subsonic-response']['randomSongs']['song']
+
+    def random_songs_from_to_year(self, from_year, to_year):
+        api_url = self.api('getRandomSongs.view',
+                           parameters={'size': '500',
+                                       'fromYear': from_year,
+                                       'toYear': to_year})
+        r = requests.get(api_url)
+        return r.json()['subsonic-response']['randomSongs']['song']
 
     def cover_art(self, id):
         return self.api('getCoverArt.view', parameters={'id': id})
@@ -83,7 +79,8 @@ class Subsonic(object):
 
 def main_page():
     menu = [{'mode': 'artist_list', 'foldername': 'Artists'},
-            {'mode': 'genre_list', 'foldername': 'Genres'}]
+            {'mode': 'genre_list', 'foldername': 'Genres'},
+            {'mode': 'random_list', 'foldername': 'Random'}]
     for entry in menu:
         url = build_url(entry)
         li = xbmcgui.ListItem(entry['foldername'],
@@ -120,8 +117,8 @@ def albums_by_genre_list():
     albums = subsonic.albums_by_genre_list(genre[0])
     for album in albums:
         url = build_url({'mode': 'track_list',
-                         'foldername': album['title'],
-                         'album_id': album['id']})
+                         'foldername': unicode(album['title']).encode('utf-8'),
+                         'album_id': unicode(album['id']).encode('utf-8')})
         li = xbmcgui.ListItem(album['artist'] + ' - ' + album['title'])
         li.setIconImage(subsonic.cover_art(album['id']))
         li.setThumbnailImage(subsonic.cover_art(album['id']))
@@ -161,8 +158,8 @@ def album_list():
     albums = subsonic.music_directory_list(artist_id[0])
     for album in albums:
         url = build_url({'mode': 'track_list',
-                         'foldername': album['title'],
-                         'album_id': album['id']})
+                         'foldername': unicode(album['title']).encode('utf-8'),
+                         'album_id': unicode(album['id']).encode('utf-8')})
         li = xbmcgui.ListItem(album['title'])
         li.setIconImage(subsonic.cover_art(album['id']))
         li.setThumbnailImage(subsonic.cover_art(album['id']))
@@ -187,6 +184,96 @@ def track_list():
                         'maxBitRate': bitrate,
                         'format': trans_format})
         li = xbmcgui.ListItem(track['title'])
+        li.setIconImage(subsonic.cover_art(track['id']))
+        li.setThumbnailImage(subsonic.cover_art(track['id']))
+        li.setProperty('fanart_image', subsonic.cover_art(track['id']))
+        li.setProperty('IsPlayable', 'true')
+        li.setInfo(
+            type='Music',
+            infoLabels={'Artist': track['artist'],
+                        'Title': track['title']})
+        xbmcplugin.addDirectoryItem(
+            handle=addon_handle,
+            url=url,
+            listitem=li)
+
+    xbmcplugin.endOfDirectory(addon_handle)
+
+
+def random_list():
+    menu = [{'mode': 'random_by_genre_list', 'foldername': 'by Genre'},
+            {'mode': 'random_from_to_year_list', 'foldername': 'from - to Year'}]
+    for entry in menu:
+        url = build_url(entry)
+        li = xbmcgui.ListItem(entry['foldername'],
+                              iconImage='DefaultFolder.png')
+        xbmcplugin.addDirectoryItem(
+            handle=addon_handle,
+            url=url,
+            listitem=li,
+            isFolder=True)
+
+    xbmcplugin.endOfDirectory(addon_handle)
+
+
+def random_by_genre_list():
+    subsonic = Subsonic(subsonic_url, username, password)
+    genres = subsonic.genre_list()
+    for genre in genres:
+        url = build_url({'mode': 'random_by_genre_track_list',
+                         'foldername': genre['value']})
+        li = xbmcgui.ListItem(genre['value'],
+                              iconImage='DefaultFolder.png')
+        xbmcplugin.addDirectoryItem(
+            handle=addon_handle,
+            url=url,
+            listitem=li,
+            isFolder=True)
+
+    xbmcplugin.endOfDirectory(addon_handle)
+
+
+def random_by_genre_track_list():
+    genre = args.get('foldername', None)[0]
+    subsonic = Subsonic(subsonic_url, username, password)
+    tracks = subsonic.random_songs_by_genre(genre)
+    for track in tracks:
+        url = subsonic.api(
+            'stream.view',
+            parameters={'id': track['id'],
+                        'maxBitRate': bitrate,
+                        'format': trans_format})
+        li = xbmcgui.ListItem(track['artist'] + ' - ' + track['title'])
+        li.setIconImage(subsonic.cover_art(track['id']))
+        li.setThumbnailImage(subsonic.cover_art(track['id']))
+        li.setProperty('fanart_image', subsonic.cover_art(track['id']))
+        li.setProperty('IsPlayable', 'true')
+        li.setInfo(
+            type='Music',
+            infoLabels={'Artist': track['artist'],
+                        'Title': track['title']})
+        xbmcplugin.addDirectoryItem(
+            handle=addon_handle,
+            url=url,
+            listitem=li)
+
+    xbmcplugin.endOfDirectory(addon_handle)
+
+
+def random_from_to_year_list():
+    dialog = xbmcgui.Dialog()
+    from_year = dialog.input('From Year', type=xbmcgui.INPUT_NUMERIC)
+    dialog = xbmcgui.Dialog()
+    to_year = dialog.input('To Year', type=xbmcgui.INPUT_NUMERIC)
+    subsonic = Subsonic(subsonic_url, username, password)
+    tracks = subsonic.random_songs_from_to_year(from_year, to_year)
+    for track in tracks:
+        url = subsonic.api(
+            'stream.view',
+            parameters={'id': track['id'],
+                        'maxBitRate': bitrate,
+                        'format': trans_format})
+        li = xbmcgui.ListItem(track['artist'] + ' - ' + track['title'])
         li.setIconImage(subsonic.cover_art(track['id']))
         li.setThumbnailImage(subsonic.cover_art(track['id']))
         li.setProperty('fanart_image', subsonic.cover_art(track['id']))
@@ -235,3 +322,11 @@ if __name__ == '__main__':
         genre_list()
     elif mode[0] == 'albums_by_genre_list':
         albums_by_genre_list()
+    elif mode[0] == 'random_list':
+        random_list()
+    elif mode[0] == 'random_by_genre_list':
+        random_by_genre_list()
+    elif mode[0] == 'random_by_genre_track_list':
+        random_by_genre_track_list()
+    elif mode[0] == 'random_from_to_year_list':
+        random_from_to_year_list()
--- plugin.audio.subsonic/test.py
+++ plugin.audio.subsonic/test.py
@@ -1,61 +1,68 @@
-import unittest
 import addon
+import json
+import mock
+import unittest
 
 
 class TestAPI(unittest.TestCase):
 
     def setUp(self):
-        self.subsonic_url = 'http://demo.subsonic.org'
-        self.username = 'guest3'
-        self.password = 'guest'
+        self.subsonic_url = 'http://foo.subsonic.bar'
+        self.username = 'foo'
+        self.password = 'bar'
+        self.subsonic = addon.Subsonic(self.subsonic_url,
+                                       self.username,
+                                       self.password)
 
-    def test_artist_list(self):
-        subsonic = addon.Subsonic(self.subsonic_url,
-                                  self.username,
-                                  self.password)
-        response = subsonic.artist_list()
+    @mock.patch('addon.requests')
+    def test_artist_list(self, mock_requests):
+        mock_requests.get.return_value = mock_response = mock.Mock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = json.load(
+            open('test/test_artist_list.json'))
+        response = self.subsonic.artist_list()
         for item in response:
             self.assertIn('name', item.keys())
             self.assertIn('id', item.keys())
 
-    def test_music_directory_list(self):
-        subsonic = addon.Subsonic(self.subsonic_url,
-                                  self.username,
-                                  self.password)
-        response = subsonic.music_directory_list(1)
+    @mock.patch('addon.requests')
+    def test_music_directory_list(self, mock_requests):
+        mock_requests.get.return_value = mock_response = mock.Mock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = json.load(
+            open('test/test_music_directory_list.json'))
+        response = self.subsonic.music_directory_list(1)
         for item in response:
             self.assertIn('artist', item.keys())
             self.assertIn('title', item.keys())
             self.assertIn('id', item.keys())
 
-    def test_cover_art(self):
-        subsonic = addon.Subsonic(self.subsonic_url,
-                                  self.username,
-                                  self.password)
-        response = subsonic.cover_art(1)
+    @mock.patch('addon.requests')
+    def test_cover_art(self, mock_requests):
+        mock_requests.get.return_value = mock_response = mock.Mock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = json.load(
+            open('test/test_music_directory_list.json'))
+        response = self.subsonic.cover_art(1)
         expected = '%s/rest/getCoverArt.view?u=%s&p=enc:%s&v=1.1.0&c=xbmc-subsonic&f=json&id=1' % (
             self.subsonic_url, self.username, self.password.encode('hex'))
         self.assertEqual(response, expected)
 
-    def test_genre_list(self):
-        subsonic = addon.Subsonic(self.subsonic_url,
-                                  self.username,
-                                  self.password)
-        response = subsonic.genre_list()
+    @mock.patch('addon.requests')
+    def test_random_songs_from_to_year(self, mock_requests):
+        mock_requests.get.return_value = mock_response = mock.Mock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = json.load(
+            open('test/test_random_songs_from_to_year.json'))
+        response = self.subsonic.random_songs_from_to_year('2006', '2007')
         for item in response:
-            self.assertIn('songCount', item.keys())
-            self.assertIn('albumCount', item.keys())
-            self.assertIn('value', item.keys())
-
-    def test_albums_by_genre_list(self):
-        subsonic = addon.Subsonic(self.subsonic_url,
-                                  self.username,
-                                  self.password)
-        response = subsonic.albums_by_genre_list('Rock')
-        for item in response:
-            self.assertIn('artist', item.keys())
-            self.assertIn('title', item.keys())
             self.assertIn('id', item.keys())
+            self.assertIn('title', item.keys())
+            self.assertIn('year', item.keys())
+            self.assertIn('genre', item.keys())
+            self.assertIn('albumId', item.keys())
+            self.assertIn('artistId', item.keys())
+            self.assertIn(int(item['year']), [2006, 2007])
 
 
 if __name__ == '__main__':
